ee7926cc57f1afa981122ece09e25bc4df8370d9
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-03 14:14:16 -0300

    Feat: Removing database columns from model.

--- app/Models/Student.ts
+++ app/Models/Student.ts
@@ -1,7 +1,5 @@
-import { column } from '@ioc:Adonis/Lucid/Orm'
 import Person from 'App/Models/Person';
 
 export default class Student extends Person {
-  @column({ isPrimary: true })
   public idStudent: number
 }
