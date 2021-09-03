77f4d1ec993756433db2425b57e326a52247393b
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-03 12:12:58 -0300

    Feat: Adding User model.

---
+++ app/Models/User.ts
@@ -0,0 +1,15 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public email: string
+
+  @column()
+  public role: boolean
+
+  @column()
+  public password: string
+}
