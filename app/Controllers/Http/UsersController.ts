1bdb1601337238fa9b2a2a06efa7413ce5ca17d4
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-04 09:54:22 -0300

    feat: changing the return of index users.

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,4 +1,42 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import Roles from 'App/Helpers/role'
 
-export default class UsersController {
+export default class UserController {
+  public async index ({ response }: HttpContextContract) {
+    const users = await User.all()
+
+    return response.status(200).json(users)
+  }
+
+  public async create ({ request, response }: HttpContextContract) {
+    let data = request.only(['email', 'role', 'password'])
+
+    const role = data.role
+
+    if (role === Roles.Student) {
+      data.role = false
+    } else if (role === Roles.Teacher) {
+      data.role = true
+    } else {
+      return response.status(400).json({ message: 'Invalid role!' })
+    }
+
+    const user = await User.create(data)
+
+    return response.status(201).json({user})
+  }
+
+  public async show ({ params }: HttpContextContract) {
+    const user = await User.findOrFail(params.id)
+
+    return user
+  }
+
+  public async destroy ({ params }: HttpContextContract) {
+    const user = await User.findOrFail(params.id)
+
+    await user.delete()
+  }
 }
+
