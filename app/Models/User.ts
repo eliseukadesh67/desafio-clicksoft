8894fd902aa21c35bd1fc869640614ea0939d2a2
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-05 00:46:38 -0300

    Feat: Adding attributes to User model.

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,27 +1,36 @@
-import { BaseModel, column, hasMany, HasMany , ManyToMany, manyToMany} from '@ioc:Adonis/Lucid/Orm'
-import Class from 'App/Models/Class';
+import { BaseModel, column, hasMany, HasMany, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import Class from 'App/Models/Class'
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
+  public name: string
+
+  @column()
+  public registration: number
+
+  @column()
+  public birthday: Date
+
+  @column()
   public email: string
 
   @column()
-  public role: boolean
+  public role: string
 
   @column()
   public password: string
 
   @hasMany(() => Class, {
-    foreignKey: 'idTeacher',
+    foreignKey: 'id_teacher',
   })
   public classes: HasMany<typeof Class>
 
   @manyToMany(() => Class, {
     localKey: 'id',
-    pivotForeignKey: 'id_class',
-    relatedKey: 'id_student',
+    pivotForeignKey: 'id_student',
+    relatedKey: 'id_class',
     pivotRelatedForeignKey: 'id_class',
     pivotTable: 'class_students',
   })
