54d781b68cc499f39478d43e724532bfce19cd51
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-05 00:40:49 -0300

    Adding methods to UserController

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,36 +1,58 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
-import Roles from 'App/Helpers/role'
+import { Roles } from 'App/Helpers/role'
 
 export default class UserController {
   public async index ({ response }: HttpContextContract) {
-    const users = await User.all()
+    try {
+      const users = await User.all()
 
-    return response.status(200).json(users)
+      if (!users) {
+        return response.status(404).json({ 'message': 'No content' })
+      }
+
+      return response.status(201).json(users)
+    } catch (error) {
+      return response.status(400).json({ 'message': error.message })
+    }
   }
 
   public async create ({ request, response }: HttpContextContract) {
-    let data = request.only(['email', 'role', 'password'])
+    try {
+      const {
+        name,
+        registration,
+        birthday,
+        email,
+        role,
+        password,
+      } = request.only(['name', 'registration', 'birthday', 'email', 'role', 'password'])
 
-    const role = data.role
+      const exists = await User.findBy('email', email)
 
-    if (role === Roles.Student) {
-      data.role = false
-    } else if (role === Roles.Teacher) {
-      data.role = true
-    } else {
-      return response.status(400).json({ message: 'Invalid role!' })
+      if (exists) {
+        return response.status(409).json({ 'message': 'Email already exists!' })
+      }
+
+      const chosen_role = Roles[role]
+
+      const data = { name, registration, birthday, email, role: chosen_role, password }
+
+      const user = await User.create(data)
+      return response.status(201).json(user)
+    } catch (error) {
+      return response.status(400).json({ 'message': error.message })
     }
-
-    const user = await User.create(data)
-
-    return response.status(201).json({user})
   }
 
-  public async show ({ params }: HttpContextContract) {
-    const user = await User.findOrFail(params.id)
+  public async show ({ params, response }: HttpContextContract) {
+    try {
+      const user = await User.findOrFail(params.id)
 
-    return user
+      return response.status(200).json(user)
+    } catch (error) {
+      return response.status(400).json({ 'message': error.message })
+    }
   }
 
   public async destroy ({ params }: HttpContextContract) {
