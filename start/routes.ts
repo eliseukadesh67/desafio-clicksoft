320dc8221caa50f4baea13b7f9b17f54b0777ed8
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-09-08 00:28:48 -0300

    Updating routes.

--- start/routes.ts
+++ start/routes.ts
@@ -3,10 +3,11 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
-Route.post('/users', 'UsersController.create')
+Route.post('/user', 'UsersController.store')
 Route.get('/users', 'UsersController.index')
 Route.get('/users/:id', 'UsersController.show')
 Route.delete('/users/:id', 'UsersController.destroy')
+Route.put('/user/:id', 'UsersController.edit')
 
 Route.post('/classes', 'ClassesController.create')
 Route.post('/classes/student', 'ClassesController.addStudent')
@@ -16,4 +17,5 @@ Route.get('/class/students/:id_class', 'ClassesController.getStudentsByClass')
 Route.delete('/classes/:id', 'ClassesController.destroy')
 Route.delete('/classes/student/:id_student', 'ClassesController.removeStudent')
 Route.get('/classes/student/:id_student', 'ClassesController.getClassesbyStudent')
+Route.put('/class/:id_class', 'ClassesController.edit')
 
